2b9a027bca40a8c70cef7cb738ed06c395111822
romizb <ruchamab123@gmail.com>
2025-02-08 10:20:38 +0200

    Update app.py

--- BFE/app.py
+++ BFE/app.py
@@ -13,14 +13,14 @@ import os
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
+app = Flask(__name__)
+
 #railway trial
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:VXKBtFWnSmqSeWgVOhmlwzTPpLdfTdpo@roundhouse.proxy.rlwy.net:51773/railway")  # Uses SQLite locally
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///bfe.db")  # Uses SQLite locally
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-
-app = Flask(__name__)
 
 # SQLite Configure database
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bfe.db'
